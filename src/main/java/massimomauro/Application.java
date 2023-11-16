93ab59eece498684781b3b6cb1386d84d757395e
MrMax01 <massimo.mauro00@gmail.com>
2023-11-16 16:34:35 +0100

    commit on branch max

--- src/main/java/massimomauro/Application.java
+++ src/main/java/massimomauro/Application.java
@@ -3,6 +3,7 @@ package massimomauro;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("ciao");
         System.out.println("Hello World!");
     }
 }
